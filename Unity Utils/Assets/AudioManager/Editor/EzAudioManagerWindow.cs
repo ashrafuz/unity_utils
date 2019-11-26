dc74c113ee4b190557c33fab7aedff24068af27f
Ashraf Zaman <ashraf@ulka.games>
2019-11-26 16:04:26 +0600

    audio manager ongoing, almost figured it out, need more usability

---
+++ Unity Utils/Assets/AudioManager/Editor/EzAudioManagerWindow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class EzAudioManagerWindow : EditorWindow {
+    static List<string> allAudioAssetName = new List<string> ();
+
+    [MenuItem ("Window/EzAudioManager")]
+    static void OpenWindow () {
+        EzAudioManagerWindow window = (EzAudioManagerWindow) GetWindow (typeof (EzAudioManagerWindow));
+        window.minSize = new Vector2 (300, 300);
+
+        window.Show ();
+
+    }
+
+    private void OnGUI () {
+        if (GUILayout.Button ("Print All Audio Asset", GUILayout.Height (40))) {
+            GetAllAudioAssets ();
+        }
+    }
+
+    static void GetAllAudioAssets () {
+        string[] guids = AssetDatabase.FindAssets ("t:audioClip");
+        foreach (string guid in guids) {
+            string fname = AssetDatabase.GUIDToAssetPath (guid);
+            string rawFileName = Path.GetFileNameWithoutExtension (fname);
+            allAudioAssetName.Add (rawFileName.ToUpper ());
+        }
+
+        AudioClassCreator acc = new AudioClassCreator ("EzAudioManagerPack", allAudioAssetName);
+    }
+
+}
---
+++ Unity Utils/Assets/AudioManager/EzAudioManagerPack.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+public class EzAudioManagerPack { 
+public static string SFX_BREAKSHOT_8 ;
+public static string SFX_PUCK_HITS_PUCK_18 ;
+public static string SFX_PUCK_HITS_EDGE_3 ;
+}
