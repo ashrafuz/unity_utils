037d1020c311a1866f4b374056d7447b47f6a383
ashrafuz <rawnok02@gmail.com>
2020-01-11 16:42:56 +0600

    slow mono update

---
+++ Unity Utils/Assets/SlowMono/CustomSlowMo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AUZ_UTIL {
+
+    public class CustomSlowMo : SlowMono {
+        protected override void SlowUpdate () {
+            Debug.Log ("running slow update");
+        }
+
+        protected override void Start () {
+            SetUpdateRateInSeconds (5);
+        }
+
+        protected override void Update () {
+            base.Update ();
+            Debug.Log ("custom update is running");
+        }
+    }
+
+}
---
+++ Unity Utils/Assets/SlowMono/SlowMono.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace AUZ_UTIL {
+
+    public abstract class SlowMono : MonoBehaviour {
+
+        private float m_ElapsedTime = 0.0f;
+        private float m_StepTime = 1f;
+        private bool m_IsRunning = true;
+
+        public virtual void SetUpdateRateInSeconds (float _sec) {
+            m_StepTime = _sec;
+        }
+
+        protected abstract void SlowUpdate ();
+
+        protected virtual void Start () {
+            SetUpdateRateInSeconds (1);
+        }
+
+        protected virtual void Update () {
+            if (m_IsRunning) {
+                m_ElapsedTime += Time.deltaTime;
+                if (m_ElapsedTime > m_StepTime) {
+                    SlowUpdate ();
+                    m_ElapsedTime = 0;
+                }
+            }
+        }
+
+        //helpers
+        protected void PauseSlowUpdate () {
+            m_IsRunning = false;
+        }
+
+        protected void HaltSlowUpdate () {
+            m_IsRunning = false;
+            m_ElapsedTime = 0;
+        }
+
+        protected void RestartUpdate () {
+            m_IsRunning = true;
+            m_ElapsedTime = 0;
+        }
+    }
+
+}
